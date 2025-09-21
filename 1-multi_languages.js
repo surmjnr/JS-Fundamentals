d6084425dc591dae865177be56cd0f769f97e9e7
Banafo <surmjnr@gmail.com>
2025-09-21 15:04:45 +0000

    Add multi-line messages

---
+++ 1-multi_languages.js
@@ -0,0 +1,7 @@
+ first_line = 'C is fun';
+ second_line = 'Python is cool';
+ third_line = 'JavaScript is amazing';
+
+ console.log(first_line);
+ console.log(second_line);
+ console.log(third_line);
